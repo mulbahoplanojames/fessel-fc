19e80a40aa8e03310d63d02b9e3800fb47eb6de1
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2025-05-13 17:23:49 +0200

    feat: enhance authentication and user experience with Google login, update user dropdown image handling, and improve shop page filtering

--- src/app/(public)/shop/[id]/page.tsx
+++ src/app/(public)/shop/[id]/page.tsx
@@ -2,8 +2,13 @@ import products from "@/data/products.json";
 import SingleProductClient from "./single-product-client";
 import { Product } from "@/types/product-type";
 
-export default function ProductPage({ params }: { params: { id: string } }) {
-  const product = products.find((product) => product.id === params.id);
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const product = products.find((product) => product.id === id);
 
   return <SingleProductClient product={product as Product} />;
 }
